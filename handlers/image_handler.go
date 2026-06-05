a8c0468a953775f49a4524703197b7cf199ef2ee
parranos10 <parranos10@gmail.com>
2026-06-05 08:44:23 -0500

    Update image_handler.go

--- handlers/image_handler.go
+++ handlers/image_handler.go
@@ -1,14 +1,23 @@
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Sistema gestión de imagenes")
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, nil)
 }
 
 func GetImages(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Lista imagenes")
+
+	w.Write([]byte("Aquí se mostrarán las imágenes guardadas"))
 }
