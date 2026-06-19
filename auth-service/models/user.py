7e22a96daaec74609aaf00732105c0adef3e1f7f
parri <parranos10@gmail.com>
2026-06-19 00:57:03 -0500

    a

--- auth-service/models/user.py
+++ auth-service/models/user.py
@@ -5,9 +5,8 @@ from typing import Optional
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True)
-    email: EmailStr = Field(unique=True) # Validación estricta de correo en la BD
-    hashed_password: str = Field(max_length=255) # Espacio suficiente para el hash largo de Argon2
-
+    email: EmailStr = Field(unique=True) 
+    hashed_password: str = Field(max_length=255) 
 class UserRegister(SQLModel):
     username: str
     email: EmailStr
